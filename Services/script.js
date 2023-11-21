2347af21d848f0b197f4cad0758474e4f8d5e5ba
zoesaqib <150381341+zoesaqib@users.noreply.github.com>
2023-11-21 20:47:45 +0500

    Add files via upload

---
+++ Services/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.querySelector('ul');
+
+    mobileMenu.addEventListener('click', function () {
+        navList.classList.toggle('show');
+    });
+});
+
+
---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.querySelector('ul');
+
+    mobileMenu.addEventListener('click', function () {
+        navList.classList.toggle('show');
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Hide loader
+    var loaderWrapper = document.querySelector('.loader-wrapper');
+    loaderWrapper.style.display = 'none';
+  });
+
+
+       
+       
+        
+
+
+
